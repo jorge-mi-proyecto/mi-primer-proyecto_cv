746a5646350e9976ad565e027540dd185a363a31
jorge-mi-proyecto <jorgesosa091@gmail.com>
2023-02-27 18:52:19 -0300

    jor-g

---
+++ function.js/funciones.js
@@ -0,0 +1,43 @@
+
+document.getElementById("boton").onclick=function(){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML="34 años de edad"
+}
+
+document.getElementById("direccion").onclick=function(){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML="el corte Alderetes San Miguel de Tucuman"
+}
+
+document.getElementById("Estudios").onclick=function(){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML="secundario completo"
+}
+
+document.getElementById("nacionalidad").onclick=function(){
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML="argentino"
+}
+
+document.getElementById("telefono").onclick=function(){
+    console.log("capturamos el evento click");
+    document.getElementById("demo1").innerHTML="3816268212"
+}
+
+document.getElementById("correo").onclick=function(){
+    console.log("capturamos el evento click");
+    document.getElementById("demo1").innerHTML="jorgesosa091@gmail.com"
+}
+
+document.getElementById("experiencia").onclick=function(){
+    console.log("capturamos el evento click");
+    document.getElementById("demo1").innerHTML="cocinero 2009/2017 construccion 2018/2020 soldador 2022"
+}
+
+document.getElementById("habilidades y destreza").onclick=function(){
+    console.log("capturamos el evento click");
+    document.getElementById("demo1").innerHTML="resolucion de problemas organizacion de trabajo trabajo en equipo comunicacion verbal y escrita capacidad de liderazgo"
+}
+
+
+
